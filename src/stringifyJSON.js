c5575ade358cb986a6f9a8b956e620fb212b41ec
Preston Whitehall <pmwhitehall@gmail.com>
2019-06-28 12:35:45 -0700

    stringifyJSON implemented

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,30 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var arr = [];
+  if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
+    return "" + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    for (i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arr + ']';
+
+  } else if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    for (let key in obj) {
+      if(typeof obj[key] === 'function' || obj[key] === undefined) {
+        delete key;
+      } else {
+        arr.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + arr + '}';
+  }
 };

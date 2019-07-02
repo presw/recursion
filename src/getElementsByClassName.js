eb6f84b248d899c23fb66a6e25dadffa2c430429
Preston Whitehall <pmwhitehall@gmail.com>
2019-07-02 10:47:48 -0700

    added initial getElementsByClassName; bug: need to solve for span

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) {
+
+  // First iteration:
+  if (typeof className === 'string') {
+    let domArray = Array.from(document.body.childNodes);
+    let recObj = {
+      output: [],
+      find: className,
+      array: domArray
+    };
+    return getElementsByClassName(recObj);
+  }
+
+  // Later recursions
+  for (let i = 0; i < className.array.length; i++) {
+    if (typeof className.array[i].className === 'string' && className.array[i].className.includes(className.find)) {
+      className.output.push(className.array[i]);
+    }
+    if (className.array[i].hasChildNodes()) {
+      let passObj = {
+        output: [],
+        array: Array.from(className.array[i].childNodes),
+        find: className.find
+      }
+      className.output = className.output.concat(getElementsByClassName(passObj));
+    }
+  }
+  return className.output;
 };
